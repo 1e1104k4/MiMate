afe00e95f6ec699ad2de8d8e557b492fbc887019
leila <143362178+1e1104k4@users.noreply.github.com>
2024-02-15 12:31:55 -0600

    Initial Commit

---
+++ MiMate/MiMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiMateApp.swift
+//  MiMate
+//
+//  Created by Leila on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MiMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
